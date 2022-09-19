e9bff7c7fca5bb33e8745951c04ca2f2f253ff8e
leliso <lelisoagegnehu491@gmail.com>
2022-09-19 15:07:22 +0300

    db

---
+++ prisma/songsData.ts
@@ -0,0 +1,25 @@
+export const artistData: {
+    name: string
+    songs: any[]
+}[] = [
+    {
+        name: "Himadri patel",
+        songs: [
+            {
+                name: "Pani da rang",
+                duration: 235,
+                url: "https://soundcloud.com/himadripatel/pani-da-rang?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+            }
+        ]
+    },
+    {
+        name: "Shahrukh Shaheen",
+        songs: [
+            {
+                name: "kuch to hua hai-singham returns",
+                duration: 125,
+                url: "https://soundcloud.com/shahrukh-shaheen/kuch-to-hua-hai-singham?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
+            }
+        ]
+    }
+]
