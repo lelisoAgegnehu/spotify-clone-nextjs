4ee21b0abe45f5f9371f0c209d20d04c6ea60798
leliso <lelisoagegnehu491@gmail.com>
2022-09-21 17:47:11 +0300

    refacrore seed data

--- prisma/songsData.ts
+++ prisma/songsData.ts
@@ -1,25 +1,65 @@
 export const artistData: {
-    name: string
-    songs: any[]
+  name: string;
+  songs: any[];
 }[] = [
-    {
-        name: "Himadri patel",
-        songs: [
-            {
-                name: "Pani da rang",
-                duration: 235,
-                url: "https://soundcloud.com/himadripatel/pani-da-rang?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-            }
-        ]
-    },
-    {
-        name: "Shahrukh Shaheen",
-        songs: [
-            {
-                name: "kuch to hua hai-singham returns",
-                duration: 125,
-                url: "https://soundcloud.com/shahrukh-shaheen/kuch-to-hua-hai-singham?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"
-            }
-        ]
-    }
-]
+  {
+    name: "codeskulptor-assets",
+    songs: [
+      {
+        name: "Epoq-Lepidoptera",
+        duration: 235,
+        url: "http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg",
+      },
+    ],
+  },
+  {
+    name: "Evillaugh",
+    songs: [
+      {
+        name: "kuch to hua hai-singham returns",
+        duration: 125,
+        url: "http://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg",
+      },
+    ],
+  },
+  {
+    name: "riceracer_assets",
+    songs: [
+      {
+        name: "lose",
+        duration: 125,
+        url: "http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/lose.ogg",
+      },
+    ],
+  },
+  {
+    name: "sounddogs",
+    songs: [
+      {
+        name: "soundtrack mp3",
+        duration: 235,
+        url: "http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/soundtrack.mp3",
+      },
+    ],
+  },
+  {
+    name: "metty terry",
+    songs: [
+      {
+        name: "I suck for you",
+        duration: 125,
+        url: "http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/race2.ogg",
+      },
+    ],
+  },
+  {
+    name: "racel 1",
+    songs: [
+      {
+        name: "love forever",
+        duration: 125,
+        url: "http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/race1.ogg",
+      },
+    ],
+  },
+];
