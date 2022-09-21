52a20210ba7f3c87cb990d69c3f2360ef382c7ce
leliso <lelisoagegnehu491@gmail.com>
2022-09-21 11:31:16 +0300

    player icons

--- components/player.tsx
+++ components/player.tsx
@@ -1,5 +1,68 @@
+import { Box, Center } from "@chakra-ui/layout";
+import { ButtonGroup, IconButton } from "@chakra-ui/react";
+import ReactHowler from "react-howler";
+import {
+  MdOutlinePauseCircleFilled,
+  MdOutlinePlayCircleFilled,
+  MdOutlineRepeat,
+  MdShuffle,
+  MdSkipNext,
+  MdSkipPrevious,
+} from "react-icons/md";
 const Player = () => {
-  return <div>Player</div>;
+  return (
+    <Box>
+      <Box>{/* <ReactHowler /> */}</Box>
+      <Center color="gray.600">
+        <ButtonGroup>
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdShuffle />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="skip"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="play"
+            fontSize="40px"
+            color="white"
+            icon={<MdOutlinePlayCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="pause"
+            fontSize="40px"
+            color="white"
+            icon={<MdOutlinePauseCircleFilled />}
+          />
+          <IconButton
+            outline="none"  
+            variant="link"
+            aria-label="next"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="repeat"
+            fontSize="24px"
+            icon={<MdOutlineRepeat />}
+          />
+        </ButtonGroup>
+      </Center>
+    </Box>
+  );
 };
 
 export default Player;
